02c4d718064bd8c49d21e00a2b61c75daeb80039
Matthias Zeller <m.zeller1901@gmail.com>
2021-06-06 13:29:46 +0200

    add matlab benchmarks

---
+++ matlab/benchmark_sylvester.m
@@ -0,0 +1,41 @@
+
+
+ns = [31, 44, 63, 89, 125, 177, 251, 354, 501, 707, 1000, 1412, 1995, 2818, 3981]
+nruns = 5;
+
+time_mean = zeros(1, length(ns));
+time_std = zeros(1, length(ns));
+
+for i = 1:length(ns)
+    n = ns(i);
+    disp(n);
+    t = zeros(length(nruns), 1);
+    for k = 1:nruns
+        disp(k);
+        [A, B, C] = build_matrices(n, n);
+        tic
+        X = sylvester(A, B, C);
+        t(k) = toc;
+    end
+    time_mean(i) = mean(t);
+    time_std(i) = std(t);
+end
+
+loglog(ns, time_mean, '-o');
+hold on;
+% Plot standard deviation
+y = [ns, fliplr(ns)];
+in_between = [time_mean - time_std, fliplr(time_mean + time_std)];
+h = fill(y, in_between, 'b');
+set(h, 'facealpha', 0.4);
+
+xlabel('n');
+ylabel('time')
+
+% Save data
+dump_data = [ns', time_mean', time_std'];
+csvwrite('benchmark_sylvester.csv', dump_data)
+
+
+
+
---
+++ matlab/build_matrices.m
@@ -0,0 +1,10 @@
+
+
+function [A, B, C] = build_matrices(m, n)
+
+A = randn(m, m);
+B = randn(n, n);
+C = randn(m, n);
+
+end
+
