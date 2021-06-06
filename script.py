0fc760dfa07dbf506f0976f3049f9a643603a060
Matthias Zeller <m.zeller1901@gmail.com>
2021-06-06 12:35:17 +0200

    modify test script

--- script.py
+++ script.py
@@ -1,12 +1,19 @@
+"""
+Test script which runs algorithms on small matrices.
+This is especially convenient to run in the console: all useful functions are already imported,
+in case you want to do some quick tests.
+"""
+
+
 from time import time
 
 import numpy as np
 from scipy.linalg import schur
 from typing import Tuple, Callable
 from recursive import rtrgsyl
-from utils import build_matrices, solve_bartels_stewart, check_sol, solve_sylvester_linear
+from utils import build_matrices, solve_bartels_stewart, check_sol, solve_sylvester_linear, solve_sylvester_lapack
 
-m, n = 1000, 1000
+m, n = 100, 100
 blks = 20
 
 mx = build_matrices(m, n)
@@ -20,6 +27,7 @@ X, t_schur, t_solve, t_back = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks)
 
 print('Checking validity of solution by plugging X into equation...')
 assert check_sol(A, B, C, X)
+print('solution is correct')
 
 print('\nsolving times:')
 print(f'i) schur decomp : \t{t_schur:.3}')
@@ -29,7 +37,8 @@ print(f'iii) map back: \t\t{t_back:.3}')
 
 print('Solving Sylvester equation with Bartels Stewart and solving small systems as linear systems ...')
 
-X, t_schur, t_solve, t_back = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks, std_solver=solve_sylvester_linear)
+A, B, C = build_matrices(m, n)
+X, t_schur, t_solve, t_back = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks, std_solver=solve_sylvester_lapack)
 assert check_sol(A, B, C, X)
 
 print('\nsolving times:')
