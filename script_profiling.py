806aca49a596faf08fc266e4635aa104120475e8
Matthias Zeller <m.zeller1901@gmail.com>
2021-06-06 12:35:50 +0200

    profiling script

--- script_profiling.py
+++ script_profiling.py
@@ -1,25 +1,71 @@
+import cProfile
+import pstats
+from functools import wraps
 
-
-from benchmark_utils import log_results, vary_matrix_size, benchmark, vary_block_size
 from recursive import rtrgsyl
-from utils import solve_sylvester_scipy, build_matrices, check_sol, solve_bartels_stewart
+from utils import solve_bartels_stewart, solve_sylvester_scipy, solve_sylvester_lapack
+import numpy as np
+from numpy.random import randn
 
-m, n = 100, 100
-blks = 50
+n = 1500
+blks = 64
+A, B, C = randn(n, n), randn(n, n), randn(n, n)
 
-# A, B, C = build_matrices(100, 100)
-#
-# X, _, _, _ = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks)
-#
-# assert check_sol(A, B, C, X)
-#
-# A, B, C = build_matrices(100, 100)
-# X, _, _, _ = solve_bartels_stewart(A, B, C, solve_sylvester_scipy)
-#
-# assert check_sol(A, B, C, X)
 
-benchmark(
-    solve_fun=solve_sylvester_scipy,
-    vary_param=('dim', [(100, 100)]),
-    bartel_stewart=True
-)
+def profile(output_file=None, sort_by='cumulative', lines_to_print=None, strip_dirs=False):
+    """https://towardsdatascience.com/how-to-profile-your-code-in-python-e70c834fad89
+    A time profiler decorator.
+    Inspired by and modified the profile decorator of Giampaolo Rodola:
+    http://code.activestate.com/recipes/577817-profile-decorator/
+    Args:
+        output_file: str or None. Default is None
+            Path of the output file. If only name of the file is given, it's
+            saved in the current directory.
+            If it's None, the name of the decorated function is used.
+        sort_by: str or SortKey enum or tuple/list of str/SortKey enum
+            Sorting criteria for the Stats object.
+            For a list of valid string and SortKey refer to:
+            https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats
+        lines_to_print: int or None
+            Number of lines to print. Default (None) is for all the lines.
+            This is useful in reducing the size of the printout, especially
+            that sorting by 'cumulative', the time consuming operations
+            are printed toward the top of the file.
+        strip_dirs: bool
+            Whether to remove the leading path info from file names.
+            This is also useful in reducing the size of the printout
+    Returns:
+        Profile of the decorated function
+    """
+
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            _output_file = output_file or func.__name__ + '.prof'
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = func(*args, **kwargs)
+            pr.disable()
+            pr.dump_stats(_output_file)
+
+            with open(_output_file, 'w') as f:
+                ps = pstats.Stats(pr, stream=f)
+                if strip_dirs:
+                    ps.strip_dirs()
+                if isinstance(sort_by, (tuple, list)):
+                    ps.sort_stats(*sort_by)
+                else:
+                    ps.sort_stats(sort_by)
+                ps.print_stats(lines_to_print)
+            return retval
+
+        return wrapper
+
+    return inner
+
+
+@profile(sort_by='cumulative', lines_to_print=-1)
+def go(blks):
+    solve_bartels_stewart(A, B, C, rtrgsyl,
+                          std_solver=solve_sylvester_lapack,
+                          blks=blks)
