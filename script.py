b84eecb6635a5692ba8dbfd5e967a0156f2d441f
Matthias Zeller <m.zeller1901@gmail.com>
2021-06-06 14:05:42 +0200

    refactor test script

--- script.py
+++ script.py
@@ -11,17 +11,17 @@ import numpy as np
 from scipy.linalg import schur
 from typing import Tuple, Callable
 from recursive import rtrgsyl
-from utils import build_matrices, solve_bartels_stewart, check_sol, solve_sylvester_linear, solve_sylvester_lapack
+from utils import build_matrices, solve_bartels_stewart, check_sol, solve_sylvester_linear
 
-m, n = 100, 100
-blks = 20
+m, n = 200, 200
+blks = 64
 
 mx = build_matrices(m, n)
 A, B, C = mx
 
 print(f'Created random matrices with shapes:\n{", ".join(str(M.shape) for M in mx)}')
 
-print('Solving Sylvester equation with Bartels Stewart and rtrgsyl ...')
+print('\nSolving Sylvester equation with Bartels Stewart and rtrgsyl ...')
 
 X, t_schur, t_solve, t_back = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks)
 
@@ -35,10 +35,10 @@ print(f'ii) rtrgsyl: \t\t{t_solve:.3}')
 print(f'iii) map back: \t\t{t_back:.3}')
 
 
-print('Solving Sylvester equation with Bartels Stewart and solving small systems as linear systems ...')
+print('\nSolving Sylvester equation with Bartels Stewart and solving small systems as linear systems ...')
 
 A, B, C = build_matrices(m, n)
-X, t_schur, t_solve, t_back = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks, std_solver=solve_sylvester_lapack)
+X, t_schur, t_solve, t_back = solve_bartels_stewart(A, B, C, rtrgsyl, blks=blks, std_solver=solve_sylvester_linear)
 assert check_sol(A, B, C, X)
 
 print('\nsolving times:')
